cef274a9d61b41dc94f7402645d1dfe9f1206d1e
KadyrS <sultan.kadyrkesh@yandex.ru>
2017-08-11 12:27:13 +0600

    Добавил проект

---
+++ DrivePoly/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace DrivePoly
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonUploadFile = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.buttonSendFile = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelFileName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonUploadFile
+            // 
+            this.buttonUploadFile.Location = new System.Drawing.Point(83, 80);
+            this.buttonUploadFile.Name = "buttonUploadFile";
+            this.buttonUploadFile.Size = new System.Drawing.Size(133, 23);
+            this.buttonUploadFile.TabIndex = 0;
+            this.buttonUploadFile.Text = "Выбрать файл";
+            this.buttonUploadFile.UseVisualStyleBackColor = true;
+            this.buttonUploadFile.Click += new System.EventHandler(this.buttonUploadFile_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Отделение:";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(83, 22);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(133, 20);
+            this.textBoxName.TabIndex = 2;
+            // 
+            // buttonSendFile
+            // 
+            this.buttonSendFile.Location = new System.Drawing.Point(83, 109);
+            this.buttonSendFile.Name = "buttonSendFile";
+            this.buttonSendFile.Size = new System.Drawing.Size(133, 23);
+            this.buttonSendFile.TabIndex = 3;
+            this.buttonSendFile.Text = "Отправить файл";
+            this.buttonSendFile.UseVisualStyleBackColor = true;
+            this.buttonSendFile.Click += new System.EventHandler(this.buttonSendFile_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Файл: ";
+            // 
+            // labelFileName
+            // 
+            this.labelFileName.AutoSize = true;
+            this.labelFileName.Location = new System.Drawing.Point(83, 52);
+            this.labelFileName.Name = "labelFileName";
+            this.labelFileName.Size = new System.Drawing.Size(0, 13);
+            this.labelFileName.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 144);
+            this.Controls.Add(this.labelFileName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonSendFile);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonUploadFile);
+            this.Name = "Form1";
+            this.Text = "Отправить файл";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonUploadFile;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Button buttonSendFile;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelFileName;
+    }
+}
+
---
+++ DrivePoly/Form1.cs
@@ -0,0 +1,142 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace DrivePoly
+{
+    public partial class Form1 : Form
+    {
+        static string[] Scopes = { DriveService.Scope.Drive };
+        static string ApplicationName = "Drive API .NET Quickstart";
+        public UserCredential credential;
+        static bool StatusSelect = false;
+        static string mimeType = string.Empty;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            using (var stream =
+                new FileStream("client_secret.json", FileMode.Open, FileAccess.Read))
+            {
+                string credPath = System.Environment.GetFolderPath(
+                    System.Environment.SpecialFolder.Personal);
+                credPath = Path.Combine(credPath, ".credentials/drive-dotnet-quickstart.json");
+
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+            }
+        }
+
+        private void buttonUploadFile_Click(object sender, EventArgs e)
+        {
+            openFileDialog.ShowDialog();
+            
+            if (openFileDialog.FileName != "")
+            {
+                string fileExtension = openFileDialog.SafeFileName.Split('.')[1];
+                labelFileName.Text = openFileDialog.SafeFileName;
+
+                if (fileExtension == "docx" || fileExtension == "excel")
+                {
+                    mimeType = "application/msword";
+                    StatusSelect = true;
+                }
+                else if (fileExtension == "xls" || fileExtension == "xlsx")
+                {
+                    mimeType = "application/vnd.ms-excel";
+                    StatusSelect = true;
+                }
+                else
+                {
+                    MessageBox.Show("Выберите файл Word или Excel", "Уведомление");
+                    return;
+                }
+            }
+        }
+
+        private void UploadFile(string mimeType)
+        {
+            try
+            {
+                var service = new DriveService(new BaseClientService.Initializer()
+                {
+                    HttpClientInitializer = credential,
+                    ApplicationName = ApplicationName,
+                });
+
+                string idFolder = string.Empty;
+                
+                    var requestF = service.Files.List();
+                    requestF.Q = "mimeType='application/vnd.google-apps.folder'";
+                    requestF.Spaces = "drive";
+                    requestF.Fields = "nextPageToken, files(id, name)";
+                    var result = requestF.Execute();
+                    foreach (var file in result.Files)
+                    {
+                        if(file.Name == textBoxName.Text)
+                        {
+                            idFolder = file.Id;
+                            break;
+                        }
+                    }
+
+
+                if (idFolder == string.Empty)
+                {
+                    //Создание папки
+                    var folderMetadata = new Google.Apis.Drive.v3.Data.File()
+                    {
+                        Name = textBoxName.Text,
+                        MimeType = "application/vnd.google-apps.folder"
+                    };
+                    var requestFolder = service.Files.Create(folderMetadata);
+                    requestFolder.Fields = "id";
+                    var folder = requestFolder.Execute();
+                    idFolder = folder.Id;
+                }
+
+                Google.Apis.Drive.v3.Data.File fileMetadata = new Google.Apis.Drive.v3.Data.File()
+                {
+                    Name = textBoxName.Text + " " + DateTime.Now,
+                    Parents = new List<string>
+                {
+                    idFolder
+                }
+                };
+
+                //Загрузка файла
+                FilesResource.CreateMediaUpload request;
+                using (var stream = new System.IO.FileStream(openFileDialog.FileName, FileMode.Open))
+                {
+                    request = service.Files.Create(fileMetadata, stream, mimeType);
+                    request.Fields = "id";
+                    request.Upload();
+                    MessageBox.Show("Файл загружен", "Уведомление");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Что-то пошло не так\nПопробуйте отправить позже", "Ошибка");
+            }
+        }
+
+        private void buttonSendFile_Click(object sender, EventArgs e)
+        {
+            if (StatusSelect)
+                UploadFile(mimeType);
+            else
+                MessageBox.Show("Вы не выбрали файл!", "Уведомление");
+        }
+    }
+}
